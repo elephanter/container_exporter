580692cdc9f422cac9ae9841d152a338a1a58f32
Eremeev Danil <hole666@mail.ru>
2015-06-08 15:56:09 +0500

    added redis and mongo stats

--- main.go
+++ main.go
@@ -6,19 +6,25 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/golang/glog"
+	"github.com/dcu/mongodb_exporter/collector"
+	"github.com/dcu/mongodb_exporter/shared"
+	"github.com/elephanter/redis_exporter/exporter"
 	"github.com/fsouza/go-dockerclient"
+	"github.com/golang/glog"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 var (
-	listeningAddress = flag.String("telemetry.address", ":9104", "Address on which to expose metrics.")
-	metricsEndpoint  = flag.String("telemetry.endpoint", "/metrics", "Path under which to expose metrics.")
-	addr             = flag.String("addr", "unix:///var/run/docker.sock", "Docker address to connect to")
-	parent           = flag.String("parent", "/docker", "Parent cgroup")
-	authUser         = flag.String("auth.user", "", "Username for basic auth.")
-	authPass         = flag.String("auth.pass", "", "Password for basic auth. Enables basic auth if set.")
-	labelString      = flag.String("labels", "", "A comma seperated list of docker labels to export for containers.")
+	listeningAddress   = flag.String("telemetry.address", ":9104", "Address on which to expose metrics.")
+	metricsEndpoint    = flag.String("telemetry.endpoint", "/metrics", "Path under which to expose metrics.")
+	addr               = flag.String("addr", "unix:///var/run/docker.sock", "Docker address to connect to")
+	redisAddr          = flag.String("redis.addr", "localhost:6379", "Address of one or more redis nodes, comma separated")
+	mongodbAddr        = flag.String("mongodb.addr", "mongodb://localhost:27017", "Mongodb URI, format: [mongodb://][user:pass@]host1[:port1][,host2[:port2],...][/database][?options]")
+	enabledMongoGroups = flag.String("groups.enabled", "asserts,durability,background_flushing,connections,extra_info,global_lock,index_counters,network,op_counters,op_counters_repl,memory,locks,metrics", "Comma-separated list of groups to use, for more info see: docs.mongodb.org/manual/reference/command/serverStatus/")
+	parent             = flag.String("parent", "/docker", "Parent cgroup")
+	authUser           = flag.String("auth.user", "", "Username for basic auth.")
+	authPass           = flag.String("auth.pass", "", "Password for basic auth. Enables basic auth if set.")
+	labelString        = flag.String("labels", "", "A comma seperated list of docker labels to export for containers.")
 )
 
 type basicAuthHandler struct {
@@ -52,8 +58,19 @@ func main() {
 	if err != nil {
 		log.Fatalf("Unable to start docker client %v", err.Error())
 	}
-	exporter := NewExporter(manager, *dockerClient, labels)
-	prometheus.MustRegister(exporter)
+
+	docker_exporter := NewExporter(manager, *dockerClient, labels)
+	prometheus.MustRegister(docker_exporter)
+
+	redisExporter := exporter.NewRedisExporter(strings.Split(*redisAddr, ","), "redis")
+	prometheus.MustRegister(redisExporter)
+
+	shared.LoadGroupsDesc()
+	shared.ParseEnabledGroups(*enabledMongoGroups)
+	mongodbCollector := collector.NewMongodbCollector(collector.MongodbCollectorOpts{
+		URI: *mongodbAddr,
+	})
+	prometheus.MustRegister(mongodbCollector)
 
 	log.Printf("Starting Server: %s", *listeningAddress)
 	handler := prometheus.Handler()
